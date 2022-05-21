d41895d08527974fb137fb09aa4049fee7cefd52
aliramadhan <yuukikimisaki@gmail.com>
2022-05-21 12:23:28 +0700

    Initial commit

---
+++ app/Http/Livewire/Product.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Product as ModelProduct;
+
+class Product extends Component
+{
+
+    public $products, $product, $price, $image, $description, $name;
+    public $isModalOpen = 0;
+
+    public function render(){
+        $this->products = ModelProduct::all();
+        return view('livewire.Product.product');
+    }
+    public function create(){
+        $this->resetCreateForm();
+        $this->openModalPopover();
+    }
+    public function openModalPopover(){
+        $this->isModalOpen = true;
+    }
+    public function closeModalPopover(){
+        $this->isModalOpen = false;
+    }
+    private function resetCreateForm(){
+        $this->name = '';
+        $this->price = '';
+        $this->image = '';
+        $this->description = '';
+    }
+    public function store(){
+        $this->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+        ]);
+    
+        Student::updateOrCreate(['id' => $this->product->id], [
+            'name' => $this->name,
+            'email' => $this->email,
+            'mobile' => $this->mobile,
+        ]);
+        session()->flash('message', $this->product->id ? 'Student updated.' : 'Student created.');
+        $this->closeModalPopover();
+        $this->resetCreateForm();
+    }
+}
